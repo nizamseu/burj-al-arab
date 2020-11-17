2b1e0c52688ff80250f768400bcce399e4f07044
nizamseu <nizamcse.seu@gmail.com>
2020-11-17 08:53:03 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Header from './componant/Header/Header';
+import Book from './componant/Book/Book';
+import Home from './componant/Home/Home';
+import Login from './componant/Login/Login';
+import Room from './componant/Room/Room';
+import PrivateRouter from './componant/PrivateRouter/PrivateRouter';
+
+export const userContext = createContext()
 
 function App() {
+   const [loggedInUser,setLoggedInUser]=useState({});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <userContext.Provider value={[loggedInUser,setLoggedInUser]}>
+      <Router>
+       <Header />
+          <Switch>
+            <Route path="/home">
+              <Home />
+            </Route>
+
+            <PrivateRouter path="/book/:bedType">
+                <Book />
+            </PrivateRouter>
+
+            <Route path="/login">
+              <Login></Login>
+            </Route>
+
+            <Route path="/room">
+              <Room />
+            </Route>
+
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+      </Router>
+      </userContext.Provider>
     </div>
   );
 }
